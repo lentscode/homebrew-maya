01607a8b2363dde175cdeeca39c68fad167b4bd4
Lentscode <antonio.lentini2004@gmail.com>
2025-01-06 19:34:06 +0100

    :sparkles: Add MacOS Intel support

--- Formula/maya.rb
+++ Formula/maya.rb
@@ -1,21 +1,26 @@
 class Maya < Formula
   desc "A CLI for developers"
   homepage "https://github.com/lentscode/maya"
-  version "0.1.3"
+  version "0.0.1"
 
-  if OS.mac?
-    url "https://github.com/lentscode/maya/releases/download/v#{version}/maya-macos-amd64"
-    sha256 "25ce12c669f0324e3104bb3f90f101dd42fea73ba3227462f89e6a01c018a0d0"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/lentscode/maya/releases/download/v#{version}/maya-macos-x86_64"
+    sha256 "9a4cbb522e22b7022c41d2abcfdaed20f4aa295eb9f91a94e85f46a9b6e898d3"
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/lentscode/maya/releases/download/v#{version}/maya-macos-arm64"
+    sha256 "d4cca69cd79e1fdb785afcbefe6fe26caf389773730723ecfdc568b244a4e05c"
   elsif OS.linux?
-    url "https://github.com/lentscode/maya/releases/download/v#{version}/maya-linux-amd64"
+    url "https://github.com/lentscode/maya/releases/download/v#{version}/maya-linux-arm64"
     sha256 "7128c2d4c4670276a309974286c16e3dba37432cfeca00ebddc8c54f06387faf"
   end
 
   def install
-    if OS.mac?
-      bin.install "maya-macos-amd64" => "maya"
+    if OS.mac? && Hardware::CPU.intel?
+      bin.install "maya-macos-x86_64" => "maya"
+    elsif OS.mac? && Hardware::CPU.arm?
+      bin.install "maya-macos-arm64" => "maya"
     elsif OS.linux?
-      bin.install "maya-linux-amd64" => "maya"
+      bin.install "maya-linux-arm64" => "maya"
     end
   end
 
